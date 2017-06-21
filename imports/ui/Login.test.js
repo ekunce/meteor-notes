77efb0be0f5b47f50a897ff4029c4c2b9fb3b01a
Eduard Kunce <eduard.kunce@gmail.com>
2017-06-21 15:28:32 +0200

    more login test cases

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -17,8 +17,36 @@ if (Meteor.isClient) {
 
       wrapper.setState({ error: '' });
       expect(wrapper.find('p').length).toBe(0);
-
     }); // it
+
+    it('should call loginWithPassword with the form data', function () {
+      const email = 'eduard@test.com';
+      const password = 'password123';
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+      wrapper.ref('email').node.value = email;
+      wrapper.ref('password').node.value = password;
+      wrapper.find('form').simulate('submit');
+
+      expect(spy.calls[0].arguments[0]).toEqual({ email });
+      expect(spy.calls[0].arguments[1]).toBe(password);
+    }); // it
+
+    it('should set loginWithPassword callback errors', function () {
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+      wrapper.find('form').simulate('submit');
+
+      spy.calls[0].arguments[2]({}); // call the err function
+      expect(wrapper.state('error')).toNotBe('');
+
+      spy.calls[0].arguments[2](); // call the err function
+      expect(wrapper.state('error')).toBe('');
+
+
+    }); //it
 
   }); // describe
 }
