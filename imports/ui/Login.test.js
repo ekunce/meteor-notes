1b70b2e8792484baf4ec48373e28bbbc710bd2eb
Eduard Kunce <eduard.kunce@gmail.com>
2017-06-21 11:42:00 +0200

    add login tests

---
+++ imports/ui/Login.test.js
@@ -0,0 +1,24 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import { Login } from './Login';
+
+if (Meteor.isClient) {
+  describe('Login', function () {
+
+    it('should show error messages', function () {
+      const error = 'This is not working';
+      const wrapper = mount(<Login loginWithPassword={() => {}}/>);
+
+      wrapper.setState({ error });
+      expect(wrapper.find('p').text()).toBe(error);
+
+      wrapper.setState({ error: '' });
+      expect(wrapper.find('p').length).toBe(0);
+
+    }); // it
+
+  }); // describe
+}
